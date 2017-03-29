5054e2f5a631e99b60089d41a4db3b5ee3c1f55c
CalvinMogodi <Calvin.Mogodi@eoh.co.za>
2017-03-29 11:08:03 +0200

    adding clubs

--- FunTime/www/homevm.js
+++ FunTime/www/homevm.js
@@ -1,1 +1,8 @@
-﻿
+﻿function homevm() {
+    var self = this;
+    var db = new Firebase("https://fun-time-9c827.firebaseio.com/Business");
+
+    function GetBusinesses(data) {
+        var business = data.val();
+    };
+}
